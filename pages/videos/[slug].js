c6e4c097c25fc31c6762647356cf5acf03e40ca7
Ankit Grai <grayankit@gmail.com>
2022-06-05 12:39:40 +0530

    further dsigning the video page
    1.added video that will be added from youtube

--- pages/videos/[slug].js
+++ pages/videos/[slug].js
@@ -1,37 +1,45 @@
-import React from 'react'
-import { useRouter } from 'next/dist/client/router'
-import Head from 'next/head';
+import React from "react";
+import { useRouter } from "next/dist/client/router";
+import Head from "next/head";
 
 function Slug() {
-    const router = useRouter();
-    const { slug } = router.query;
+  const router = useRouter();
+  const { slug } = router.query;
   return (
     <div>
-        <Head>
+      <Head>
         <title>{slug} | Grayankit.me</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="h-screen">
-          <div className='flex flex-col border-2 border-black float-right h-5/6 overflow-scroll w-2/6'>
-              <ul className="list-none">
-                  <li className='border-2 border-purple-600 text-lg text-gray-600 text-center m-2 hover:cursor-pointer hover:text-white hover:bg-gray-600'>
-                      <span className="text-lg">Introduction to python</span>
-                  </li>
-                  <li className='border-2 border-purple-600 text-lg text-gray-600 text-center m-2 hover:cursor-pointer hover:text-white hover:bg-gray-600'>
-                      <span className="text-lg">Python Installation</span>
-                  </li>
-                  <li className='border-2 border-purple-600 text-lg text-gray-600 text-center m-2 hover:cursor-pointer hover:text-white hover:bg-gray-600'>
-                      <span>Data types in python</span>
-                  </li>
-              </ul>
-          </div>
-          {/* for displaying the video */}
-          <div className='border-black border-2 max-w-fitxl h-96 overflow-auto'>
-              <h1>hello world!</h1>
-          </div>
+        <div className="md:flex lg:flex hidden flex-col border-2 border-black float-right h-5/6 overflow-scroll w-2/6">
+          <ul className="list-none">
+            <li className="border-2 border-purple-600 text-lg text-gray-600 text-center m-2 hover:cursor-pointer hover:text-white hover:bg-gray-600">
+              <span className="text-lg">Introduction to python</span>
+            </li>
+            <li className="border-2 border-purple-600 text-lg text-gray-600 text-center m-2 hover:cursor-pointer hover:text-white hover:bg-gray-600">
+              <span className="text-lg">Python Installation</span>
+            </li>
+            <li className="border-2 border-purple-600 text-lg text-gray-600 text-center m-2 hover:cursor-pointer hover:text-white hover:bg-gray-600">
+              <span>Data types in python</span>
+            </li>
+          </ul>
+        </div>
+        {/* for displaying the video */}
+        <div className="border-black border-2 max-w-fitxl md:h-4/5 lg:h-4/5 overflow-hidden h-72">
+          <iframe
+            src="https://www.youtube.com/embed/rmIoA9DlaK0"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="jsx-2880253090 w-full h-full"
+          />
+        </div>
+        {/* Start building navbar of content from here */}
       </section>
     </div>
-  )
+  );
 }
 
-export default Slug
+export default Slug;
